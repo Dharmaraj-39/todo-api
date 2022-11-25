ebb4bc6dfdf425d9ee28afe3f6e342379b36f22a
Dharmaraj <dharmaraj.m@stinsoft.com>
2022-11-25 11:12:35 +0530

    added todo get by id

--- server.js
+++ server.js
@@ -4,8 +4,46 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description: 'Wake up at 6 AM',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to Gym',
+    completed: false
+}, {
+    id: 3,
+    description: 'Done a Meditation',
+    completed: true
+}];
+
 app.get('/', function (req, res) {
     res.send('Todo API...');
+});
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+    todos.forEach(function (todo) {
+
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+    
 });
 
 app.listen(PORT, function () {
