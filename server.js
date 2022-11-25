0041877ca50ef0682725ac4e42e7f8d57fe6d146
Dharmaraj <dharmaraj.m@stinsoft.com>
2022-11-25 13:07:16 +0530

    added post todo

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    description: 'Wake up at 6 AM',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to Gym',
-    completed: false
-}, {
-    id: 3,
-    description: 'Done a Meditation',
-    completed: true
-}];
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API...');
@@ -44,6 +35,16 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
     
+});
+
+app.post('/todos', function(req, res) {
+
+    var body = req.body;
+    
+    body.id = todoNextId++;
+    todos.push(body);
+    res.send(body);
+
 });
 
 app.listen(PORT, function () {
