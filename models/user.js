6cc903117db9b8ae52a64c8eabb4b529382ae904
Dharmaraj <dharmaraj.m@stinsoft.com>
2022-11-30 14:24:10 +0530

    Added salt and hash passwords

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
+ var bcrypt = require('bcrypt');
+ var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
 
-    return sequelize.define('user' ,{
+    var user = sequelize.define('user' ,{
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -9,13 +11,42 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 isEmail: true
             }
+        }, 
+        salt: {
+            type: DataTypes.STRING
+        }, 
+        password_hash: {
+            type: DataTypes.STRING
         },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [6, 30]
+            },
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+            }
+        }
+    }, {
+        hooks: {    //validate the input before validation by hooks
+            beforeValidate: function (user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
             }
         }
     });
+
+    user.prototype.toPublicJSON = function () {
+        var json = this.toJSON();
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+    }
+
+    return user;
 };
